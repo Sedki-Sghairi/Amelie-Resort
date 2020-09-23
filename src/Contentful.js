676d4c8b1754b0da4d6311c6add50cb762208d86
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-09-23 12:35:53 +0100

    depleyment on netlify

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,5 +1,5 @@
 import { createClient } from 'contentful';
 export default createClient({
-	space: process.env.REACT_APP_AMELIE_API,
+	space: process.env.REACT_APP_API_SPACE,
 	accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
